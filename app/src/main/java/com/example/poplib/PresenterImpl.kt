83bebfc68f842da1e2b087b48fcaaf4b354c96fd
whittema <shamarsen@mail.ru>
2023-03-20 10:27:17 +0300

    Устранено замечание по использованию магических чисел

--- app/src/main/java/com/example/poplib/PresenterImpl.kt
+++ app/src/main/java/com/example/poplib/PresenterImpl.kt
@@ -7,22 +7,28 @@ class PresenterImpl:Contract.Presenter {
 
 
     override fun clickButton1() {
-        counter.increaseValue(1)
-        mainView?.setValueButton1(counter.getValue(1).toString())
+        counter.increaseValue(INDEX_COUNTER_1)
+        mainView?.setValueButton1(counter.getValue(INDEX_COUNTER_1).toString())
 
     }
 
     override fun clickButton2() {
-        counter.increaseValue(2)
-        mainView?.setValueButton2(counter.getValue(2).toString())
+        counter.increaseValue(INDEX_COUNTER_2)
+        mainView?.setValueButton2(counter.getValue(INDEX_COUNTER_2).toString())
     }
 
     override fun clickButton3() {
-        counter.increaseValue(3)
-        mainView?.setValueButton3(counter.getValue(3).toString())
+        counter.increaseValue(INDEX_COUNTER_3)
+        mainView?.setValueButton3(counter.getValue(INDEX_COUNTER_3).toString())
     }
 
     override fun onAttach(mView: Contract.MainView) {
         mainView = mView
+    }
+
+    companion object{
+        const val INDEX_COUNTER_1 = 1
+        const val INDEX_COUNTER_2 = 2
+        const val INDEX_COUNTER_3 = 3
     }
 }
